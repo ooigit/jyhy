e1e8015cd5b03bfa038cee24014c09f02498128b
ooigit <ooiregister@gmail.com>
2012-01-07 08:27:07 +0800

    changed footer style

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,4 @@
-<footer class="span-24 last"><br />
+<footer id="footer" class="span-24 last"><br />
 <span>Copyright 2009-2010 All Rights Reserved</span><br />
 <span>公司地址:...... 电话：.......... 手机:....... 邮政编码:..... 备案号</span>
 </footer>
